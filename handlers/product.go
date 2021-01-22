4f6406361d00a2c1580ae450dbe923ee7716940d
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-01-22 23:42:46 +0530

    product write

--- handlers/product.go
+++ handlers/product.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/Binuraowin/go-microservices/data"
 )
@@ -28,6 +30,33 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		p.addProducts(rw, r)
 		return
+	}
+	if r.Method == http.MethodPut {
+		p.l.Println("PUT", r.URL.Path)
+		// expect the id in the URI
+		reg := regexp.MustCompile(`/([0-9]+)`)
+		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(g) != 1 {
+			p.l.Println("Invalid URI more than one id")
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		if len(g[0]) != 2 {
+			p.l.Println("Invalid URI more than one capture group")
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			p.l.Println("Invalid URI unable to convert to numer", idString)
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+		p.l.Println("hj",id)
 	}
 
 	// catch all
@@ -60,6 +89,6 @@ func (p *Products) addProducts(rw http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusBadRequest)
 	}
-	data.AddProduct(prod)
+	data.AddProduct(prod )
 	p.l.Printf("%#v",prod)
 }
